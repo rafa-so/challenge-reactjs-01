3d1dfd04fcb9a22d585b6c4f4b0e1302504531a6
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-06-22 23:07:28 -0300

    Fix implement to methods to pass tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// import { uuid } from 'uuidv4';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+      api.get('repositories')
+      .then(response => {
+        setRepos(response.data);
+      });   
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      url: 'https://api.github.com/users/rafa-so',
+      title: `Desafio ReactJS ${Date.now()}`,
+      techs: [ 'React' ]
+    })
+
+    setRepos([ ...repos, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const filtred = repos.filter(repo => repo.id !== id);
+
+    setRepos(filtred);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repos &&
+          repos.map((repos) => {
+            return (
+              <li key={ repos.id }>
+                { repos.title }
+      
+                <button onClick={() => handleRemoveRepository(repos.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
